9b90797fc59033109bf4486116fe9e263413d29b
Rudy Chin <bb1168kk@gmail.com>
2017-06-12 17:10:26 +0800

    add new volume for easily testing

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-MODEL_PATH='data/rudychin/face-swap/data/models'
+MODEL_PATH='/data/rudychin/face-swap/data/models'
+IMAGE_PATH='/data/rudychin/face-swap/data/images'
 
 nvidia-docker run \
 	-it --rm \
 	-v ${MODEL_PATH}:/root/face_swap/data/models \
+	-v ${IMAGE_PATH}:/root/face_swap/data/images \
 	-e APP=face_swap_image \
 	-e ARGS="--cfg /root/face_swap/face_swap_image.cfg" \
 	ailabs/face-swap:cpu
