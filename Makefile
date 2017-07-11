d4cb54882cd1f0ced3301d29608af24f139bd9a0
Rudy Chin <bb1168kk@gmail.com>
2017-07-11 18:02:38 +0800

    first alpha version

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 docker-cpu: Dockerfile.cpu
-	sudo docker build -t ailabs/face-swap:cpu -f $< .
+	docker build -t ailabs/face-swap:cpu -f $< .
 
 docker-gpu: Dockerfile.gpu
-	sudo docker build -t ailabs/face-swap:gpu -f $< .
+	docker build -t ailabs/face-swap:gpu -f $< .
 
--- run-gpu.sh
+++ run-gpu.sh
@@ -8,14 +8,18 @@ TEST_PROGRAM='/usr/bin/python /root/face_swap/py_face_swap/pytest.py'
 XSOCK=/tmp/.X11-unix
 
 nvidia-docker run \
+    -it \
 	--network=host \
 	--privileged \
 	-v $XSOCK:$XSOCK:rw \
 	-v ~/.Xauthority:/.Xauthority:rw \
-	-e DISPLAY=:2 \
+	-e DISPLAY=:0 \
 	-e XAUTHORITY=/.Xauthority \
+    -p $1:$1 \
 	-v ${MODEL_PATH}:/root/face_swap/data/models \
 	-v ${IMAGE_PATH}:/root/face_swap/data/images \
 	-v ${OUTPUT_PATH}:/root/face_swap/data/output \
+    $2 \
     ailabs/face-swap:gpu \
-    ${TEST_PROGRAM}
+    /bin/bash
+    #${TEST_PROGRAM}
